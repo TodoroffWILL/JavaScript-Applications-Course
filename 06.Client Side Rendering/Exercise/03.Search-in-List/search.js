b8e1b5c1c778e46a28a1135bd24b1648755ff162
Doncho Todorov <108018258+TodoroffWILL@users.noreply.github.com>
2023-03-30 19:32:30 +0300

    task 4 done

--- 06.Client Side Rendering/Exercise/03.Search-in-List/search.js
+++ 06.Client Side Rendering/Exercise/03.Search-in-List/search.js
@@ -1,3 +1,28 @@
+import { towns } from './towns.js';
+import { html, render } from '../../../node_modules/lit-html/lit-html.js';
+
+const root = document.getElementById('towns');
+
+const template = (towns) =>
+  html`<ul>
+    ${towns.map((x) => html`<li>${x}</li>`)}
+  </ul>`;
+
+render(template(towns), root);
+
+document.querySelector('button').addEventListener('click', search);
+
 function search() {
-   // TODO
+  const searchText = document.getElementById('searchText').value;
+  const allTowns = Array.from(document.querySelectorAll('li'));
+
+  const found = allTowns.filter((x) => x.textContent.includes(searchText));
+  if (found.length > 0) {
+    found.forEach((x) => {
+      x.classList.add('active');
+    });
+  }
+  document.getElementById(
+    'result'
+  ).textContent = `${found.length} matches found`;
 }
--- 06.Client Side Rendering/Exercise/04.Fill-Dropdown/dropdown.js
+++ 06.Client Side Rendering/Exercise/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,50 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from '../../../node_modules/lit-html/lit-html.js';
+
+const getUrl = 'http://localhost:3030/jsonstore/advanced/dropdown';
+
+async function getOptions() {
+  try {
+    const response = await fetch(getUrl);
+    if (!response.ok) {
+      throw new Error(response.message);
+    }
+    return await response.json();
+  } catch (error) {
+    alert(error.message);
+  }
 }
+
+const template = (data) => html`
+  ${data.map((el) => html`<option value=${el._id}>${el.text}</option>`)}
+`;
+
+const options = Object.values(await getOptions());
+const root = document.getElementById('menu');
+
+// Add new Element
+
+document.querySelector('form').addEventListener('submit', addItem);
+
+async function addItem(e) {
+  e.preventDefault();
+  const text = document.getElementById('itemText').value;
+  try {
+    if (text != '') {
+      const response = await fetch(getUrl, {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({ text }),
+      });
+      if (!response.ok) {
+        throw new Error(response.message);
+      }
+      const data = await response.json();
+      options.push(data);
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+  document.getElementById('itemText').value = '';
+  render(template(options), root);
+}
+render(template(options), root);
